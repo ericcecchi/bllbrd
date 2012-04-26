35aab40834b3944543f475f914f36a9b566517ee
Eric Cecchi <eric.cecchi@gmail.com>
2012-04-26 14:54:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bllbrd::Application.config.secret_token = '920a54db095346207aaefa664cc35d35b15a2325e38d503c9d48dc4dede2c3696ee832465c029f2989fdf5e3d80374a418a961f036f1cd83d696043deef5d05d'
