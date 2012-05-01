682872b95c35909caec2fc6c183fa8b4e898a53b
Eric Cecchi <eric.cecchi@gmail.com>
2012-05-01 00:22:10 -0500

    Restructured stylesheets, added login redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 	  auth = request.env["omniauth.auth"]
 	  user = User.where(provider: auth["provider"], uid: auth["uid"]).first || User.create_with_omniauth(auth)
 	  session[:user_id] = user.id
-	  redirect_to root_url, :notice => "Signed in!"
+	  redirect_to songs_url, :notice => "Signed in!"
 	end
 	
 	def destroy
